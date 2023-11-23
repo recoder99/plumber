466318b5fc769e9d6dc80db18bd663bf998606db
recoder99 <30973326+recoder99@users.noreply.github.com>
2023-11-23 19:20:51 +0800

    Unfinished Lexer. Enums added

--- src/main/java/org/recoder/Lexer.java
+++ src/main/java/org/recoder/Lexer.java
@@ -1,2 +1,71 @@
-package org.recoder;public class Lexer {
+package org.recoder;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lexer {
+
+    List<Token> tokensList = new ArrayList<>();
+    String filepath;
+
+    Lexer(String filepath){
+        this.filepath = filepath;
+    }
+
+    public void catFile(String filepath) throws IOException {
+        FileReader reader = new FileReader(filepath);
+        int b;
+        while((b=reader.read()) != -1){
+            System.out.print((char)b);
+        }
+    }
+
+    //Scan for each character
+    public void ScanToken(String text) throws  IOException{
+        FileReader reader;
+        try {
+            reader = new FileReader(filepath);
+        } catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+        char b;
+        int line = 0;
+
+        while ((b=(char)reader.read()) != -1){
+            switch (b){
+                case '+':
+                    addToken(TokenType.PLUS, "+", line);
+                    break;
+                case '-':
+                    addToken(TokenType.MINUS, "-",  line);
+                    break;
+                case '*':
+                    addToken(TokenType.STAR, "*",  line);
+                    break;
+                case '/':
+                    addToken(TokenType.SLASH, "/",  line);
+                    break;
+                case '(':
+                    addToken(TokenType.LPAREN, "(",  line);
+                    break;
+                case ')':
+                    addToken(TokenType.RPAREN, ")",  line);
+                    break;
+                case ';':
+                    addToken(TokenType.SEMICOL, ";",  line);
+                    break;
+                case ':':
+                    addToken(TokenType.COL, ":",  line);
+                    break;
+            }
+        }
+    }
+
+
+    //add token
+    private void addToken(TokenType type, String lexeme, int line){
+        tokensList.add(new Token(type, lexeme, line));
+    }
 }
--- src/main/java/org/recoder/Token.java
+++ src/main/java/org/recoder/Token.java
@@ -1,2 +1,36 @@
-package org.recoder;public class Token {
+package org.recoder;
+
+//list of tokens
+enum TokenType {
+    //single character tokens
+    LPAREN, RPAREN, COL, SEMICOL,
+    LCBRACK, RCBRACK, PLUS, MINUS, STAR, SLASH, DOT, NEWLINE, QUOTE,
+
+    //two or more character token
+    ASMT, NEQUAL, EQUAL, GT, LT, GT_EQUAL, LT_EQUAL,
+
+    //literals
+    ID, STRING, NUMBER, HASH,
+
+    //Keywords
+    GET, SET, DO, IF, ELIF, ELSE, FOR, WHILE, TRUE, FALSE,
+    EOF
+}
+
+//Token Class
+public class Token {
+    final String lexeme;
+    final TokenType type;
+    //final Object literal;
+    final int line;
+
+    Token(TokenType type, String lexeme, int line){
+        this.lexeme = lexeme;
+        this.type = type;
+        //this.literal = literal;
+        this.line = line;
+    }
+    public void ShowToken(){
+        System.out.println("('" + lexeme +"') - Token Type: " + type);
+    }
 }
