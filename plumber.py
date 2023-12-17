b1bd079c78da068dd8f1ad49faf6f39811bd192d
recoder99 <30973326+recoder99@users.noreply.github.com>
2023-12-18 02:54:46 +0800

    plumber.py minor mods. just ignore it

--- plumber.py
+++ plumber.py
@@ -5,12 +5,12 @@ def main():
 
     args_list = sys.argv
 
-    # if len(args_list) != 2: 
-    #     print("Usage: python plumber.py <input file>")
-    #     sys.exit(1)
+    if len(args_list) != 2: 
+         print("Usage: python plumber.py <input file>")
+         sys.exit(1)
 
     try: 
-        simple_lexer = LexicalAnalyzer("D:\\user_data\\Documents\\code_projects\\plumber\\sample.plumb")
+        simple_lexer = LexicalAnalyzer(args_list[1])
         simple_lexer.scanToken()
         simple_lexer.displayTokenTable()
         simple_lexer.outputTextFile()
